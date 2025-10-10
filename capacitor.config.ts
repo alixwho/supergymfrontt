c131f9c8a7522964da9b46adeb08f8c894a95404
Lu <utti222028@utvco.edu.mx>
2025-10-10 12:16:40 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'supergymfront',
+  webDir: 'www'
+};
+
+export default config;
