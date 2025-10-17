aed25e74d808c42d4e0bd6fe50885a1d4cb34e24
Lu <utti222028@utvco.edu.mx>
2025-10-16 23:54:30 -0600

    Cambio de diseño de segment a cards por dias de rutinas

--- src/app/listaejer/listaejer.page.ts
+++ src/app/listaejer/listaejer.page.ts
@@ -9,33 +9,7 @@ import { NavController } from '@ionic/angular';
 })
 export class ListaejerPage implements OnInit {
 
-  parteSeleccionada = 'hombro'; 
-
-  // Ejercicios por categoría
-  ejerciciosHombro = [
-    { nombre: 'Press Militar', series: 4, reps: 12 },
-    { nombre: 'Elevaciones Laterales', series: 3, reps: 15 },
-  ];
-
-  ejerciciosPecho = [
-    { nombre: 'Press de Banca', series: 4, reps: 10 },
-    { nombre: 'Aperturas con Mancuernas', series: 3, reps: 12 },
-
-  ];
-
-  ejerciciosPiernas = [
-    { nombre: 'Sentadillas', series: 4, reps: 12 },
-    { nombre: 'Prensa de Piernas', series: 4, reps: 10 },
-  ];
-
   constructor(private navCtrl: NavController) {}
 
   ngOnInit() {}
-
-  // Método para abrir la página de video
-  verVideo(ejercicio: any) {
-    console.log('Ver video de:', ejercicio.nombre);
-    // Luego aquí pondrás la navegación a tu otra página
-    // this.navCtrl.navigateForward('/ver-video');
-  }
 }
