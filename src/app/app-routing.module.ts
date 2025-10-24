3323b6f6652bccd0b1d8cd75245b54df4a51712a
Lu <utti222028@utvco.edu.mx>
2025-10-23 21:12:19 -0600

    eliminar carpetas que no se necesitan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,36 +20,12 @@ const routes: Routes = [
     loadChildren: () => import('./listaejer/listaejer.module').then( m => m.ListaejerPageModule)
   },
   {
-    path: 'detallejer',
-    loadChildren: () => import('./detallejer/detallejer.module').then( m => m.DetallejerPageModule)
-  },
+  path: 'detallejer/:dia',
+  loadChildren: () => import('./detallejer/detallejer.module').then(m => m.DetallejerPageModule)
+},
   {
     path: 'terminarejer',
     loadChildren: () => import('./terminarejer/terminarejer.module').then( m => m.TerminarejerPageModule)
-  },
-  {
-    path: 'rutinamartes',
-    loadChildren: () => import('./rutinamartes/rutinamartes.module').then( m => m.RutinamartesPageModule)
-  },
-  {
-    path: 'rutinamiercoles',
-    loadChildren: () => import('./rutinamiercoles/rutinamiercoles.module').then( m => m.RutinamiercolesPageModule)
-  },
-  {
-    path: 'rutinajueves',
-    loadChildren: () => import('./rutinajueves/rutinajueves.module').then( m => m.RutinajuevesPageModule)
-  },
-  {
-    path: 'rutinaviernes',
-    loadChildren: () => import('./rutinaviernes/rutinaviernes.module').then( m => m.RutinaviernesPageModule)
-  },
-  {
-    path: 'rutinasabado',
-    loadChildren: () => import('./rutinasabado/rutinasabado.module').then( m => m.RutinasabadoPageModule)
-  },
-  {
-    path: 'rutinadomingo',
-    loadChildren: () => import('./rutinadomingo/rutinadomingo.module').then( m => m.RutinadomingoPageModule)
   },
 ];
 
