df0ddc0717810038d011523a0fdcd753b4579e74
Lu <utti222028@utvco.edu.mx>
2025-11-25 12:46:43 -0600

    agregar standalone para no marcar error

--- src/app/terminodedia/terminodedia.page.ts
+++ src/app/terminodedia/terminodedia.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-terminodedia',
+  standalone: false,
   templateUrl: './terminodedia.page.html',
   styleUrls: ['./terminodedia.page.scss'],
 })
